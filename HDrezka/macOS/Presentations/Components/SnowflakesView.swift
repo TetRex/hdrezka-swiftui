5ba50db691c6b08cffe73cb5445a4a8dff99b9d2
voidboost <voidboost.hdrezka@gmail.com>
2025-12-25 13:25:36 +0200

    small improvements

--- HDrezka/macOS/Presentations/Components/SnowflakesView.swift
+++ HDrezka/macOS/Presentations/Components/SnowflakesView.swift
@@ -17,17 +17,19 @@ final class SnowflakesEmitterView: NSView {
     private func setupEmitterLayer() {
         wantsLayer = true
 
-        let emitterCells = generateCGImages().map { image in
-            let emitterCell = CAEmitterCell()
-            emitterCell.contents = image
-            emitterCell.scale = 0.9
-            emitterCell.scaleRange = 0.1
-            emitterCell.birthRate = 0.2
-            emitterCell.velocity = 20
-            emitterCell.velocityRange = 10
-            emitterCell.spinRange = Angle.degrees(45).radians
-            emitterCell.emissionLongitude = Angle.degrees(0).radians
-            emitterCell.emissionRange = Angle.degrees(30).radians
+        let defaultEmitterCell = CAEmitterCell()
+        defaultEmitterCell.scale = 0.9
+        defaultEmitterCell.scaleRange = 0.1
+        defaultEmitterCell.birthRate = 0.2
+        defaultEmitterCell.velocity = 20
+        defaultEmitterCell.velocityRange = 10
+        defaultEmitterCell.spinRange = Angle.degrees(45).radians
+        defaultEmitterCell.emissionLongitude = Angle.degrees(0).radians
+        defaultEmitterCell.emissionRange = Angle.degrees(30).radians
+
+        let emitterCells = Snowflakes.cgImages.map { cgImage in
+            let emitterCell = defaultEmitterCell.copy() as! CAEmitterCell
+            emitterCell.contents = cgImage
 
             return emitterCell
         }
@@ -47,80 +49,10 @@ final class SnowflakesEmitterView: NSView {
 
         guard let emitterLayer else { return }
 
-        emitterLayer.emitterPosition = CGPoint(x: bounds.midX, y: bounds.maxY + (maxRadius * 2))
+        emitterLayer.emitterPosition = CGPoint(x: bounds.midX, y: bounds.maxY + Snowflakes.maxRadius)
         emitterLayer.emitterSize = CGSize(width: bounds.width, height: 0)
         emitterLayer.emitterCells?.forEach { emitterCell in
-            emitterCell.lifetime = Float((bounds.height + (maxRadius * 4)) / max(abs(emitterCell.velocity) - abs(emitterCell.velocityRange), 1))
-        }
-    }
-
-    private func getRadius(size: CGFloat, rectCount: Int) -> CGFloat {
-        let lastRectSize = size * pow(0.9, CGFloat(rectCount - 1))
-        let lastRectOffset = lastRectSize * 0.7 * (CGFloat(rectCount - 1) + 0.8)
-        let halfDiagonal = lastRectSize * sqrt(2) / 2
-        return lastRectOffset + halfDiagonal
-    }
-
-    private var maxRadius: CGFloat {
-        getRadius(size: 5, rectCount: 5)
-    }
-
-    private func getAngleStep(raysCount: Int) -> CGFloat {
-        360 / CGFloat(raysCount)
-    }
-
-    private func createCGImage(size: CGFloat, rectCount: Int, raysCount: Int) -> CGImage? {
-        let radius = getRadius(size: size, rectCount: rectCount)
-        let angleStep = getAngleStep(raysCount: raysCount)
-
-        guard let context = CGContext(
-            data: nil,
-            width: Int(radius * 2),
-            height: Int(radius * 2),
-            bitsPerComponent: 8,
-            bytesPerRow: 0,
-            space: CGColorSpaceCreateDeviceRGB(),
-            bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue,
-        ) else {
-            return nil
-        }
-
-        context.translateBy(x: radius, y: radius)
-
-        for ray in 0 ..< raysCount {
-            context.saveGState()
-
-            context.rotate(by: Angle.degrees(angleStep * CGFloat(ray)).radians)
-
-            for rect in 0 ..< rectCount {
-                let rectWidth = size * pow(0.9, CGFloat(rect) + 1)
-
-                context.saveGState()
-
-                context.translateBy(x: rectWidth * 0.7 * (CGFloat(rect) + 0.8), y: 0)
-                context.rotate(by: Angle.degrees(45).radians)
-                context.setFillColor(NSColor.systemBlue.withAlphaComponent(0.4).cgColor)
-                context.fill(
-                    CGRect(
-                        x: -rectWidth / 2,
-                        y: -rectWidth / 2,
-                        width: rectWidth,
-                        height: rectWidth,
-                    ),
-                )
-
-                context.restoreGState()
-            }
-
-            context.restoreGState()
-        }
-
-        return context.makeImage()
-    }
-
-    private func generateCGImages() -> [CGImage] {
-        product(2 ... 5, 5 ... 7).compactMap { rectCount, raysCount in
-            createCGImage(size: 5, rectCount: rectCount, raysCount: raysCount)
+            emitterCell.lifetime = Float((bounds.height + (Snowflakes.maxRadius * 2)) / max(abs(emitterCell.velocity) - abs(emitterCell.velocityRange), 1))
         }
     }
 }
